5aa8bc94a4299a18831c0017aeaa97d66602d21d
unknown <Kashifraza4455>
2025-08-06 17:09:42 +0500

    uptade

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors(
+      
+));
 app.use(express.json());
 
 app.post("/send-email", async (req, res) => {
